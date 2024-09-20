f3a19d79c2ba373c16a3d71ec5b0ebbf3b3b21fe
KennyAle <kennyviver@gmail.com>
2024-09-20 11:57:09 -0700

    Exercise 25

--- exercises/ex25.js
+++ exercises/ex25.js
@@ -10,3 +10,28 @@ loopy([15, 90], [2, 5], ["Batty", "Beacon"]);
 
 The loopy function should log to the console all the numbers from 15 to 90, except replacing multiples of 2 with "Batty", multiples of 5 with "Beacon", and multiples of 2 and 5 with "BattyBeacon".
 */
+function loopy(range,multiples, words){
+    for(i = range[0]; i <= range[1]; i++){
+        if (i % multiples[0] == 0 && i % multiples[1] == 0){
+            console.log(i + " " + words[0] + words[1])
+        } else if (i % multiples[0] == 0) {
+            console.log(i + " " + words[0])
+        } else if (i % multiples[1] == 0) {
+            console.log(i + " " + words[1])
+        } else {
+            console.log(`${i}`)
+        }
+    }
+}
+loopy([1,10],[2,5],["Batty","Beacon"])
+
+// let range = [1,5]
+// for (let index = range[0]; index < range[1] + 1; index++) {
+//     console.log("a")
+// }
+// function loopy(range){
+//     for (let index = range[0]; index < range[1] + 1; index++) {
+//         console.log(index)
+//     }
+// }
+// loopy([1,5])
